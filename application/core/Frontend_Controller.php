8b0af787301c29e701abeea5c9727db4ec57c09c
Trịnh Quốc Hoàng <quochoangvp@gmail.com>
2016-06-20 20:57:47 -0700

    parse media data for frontend

--- application/core/Frontend_Controller.php
+++ application/core/Frontend_Controller.php
@@ -4,8 +4,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Frontend_Controller extends Base_Controller
 {
 
-	public $theme;
-	public $layout;
+    public $theme;
+    public $layout;
 
     public function __construct()
     {
@@ -17,27 +17,32 @@ class Frontend_Controller extends Base_Controller
         $this->load->model('widget/common/widget_model', 'common_widget');
         $widgets = $this->common_widget->get_all_widget_frontend();
         foreach ($widgets as $widget) {
-        	$layouts = explode('|', $widget['layout']);
-        	$themes = explode('|', $widget['theme']);
-        	if (in_array($this->layout, $layouts) && in_array($this->theme, $themes)) {
-        		$_function = '_get_widget_data_' . $widget['type_name'];
-        		$this->$_function($widget);
-        	}
+            $layouts = explode('|', $widget['layout']);
+            $themes = explode('|', $widget['theme']);
+            if (in_array($this->layout, $layouts) && in_array($this->theme, $themes)) {
+                $_function = '_get_widget_data_' . $widget['type_name'];
+                $this->$_function($widget);
+            }
         }
     }
 
     private function _get_widget_data_nav($widget)
     {
-    	$this->load->model('category/common/category_model', 'common_category');
-    	$widget_categories = json_decode($widget['config'])->categories;
-    	$categories = $this->common_category->get_category_nested_frontend($widget_categories);
-    	$this->load->section($widget['position_name'], ['data' => $categories]);
+        $this->load->model('category/common/category_model', 'common_category');
+        $widget_categories = json_decode($widget['config'])->categories;
+        $categories = $this->common_category->get_category_nested_frontend($widget_categories);
+        $this->load->section($widget['position_name'], ['data' => $categories]);
     }
 
     private function _get_widget_data_media($widget)
     {
-    	$medias = [];
-    	$this->load->section($widget['position_name'], ['data' => $medias]);
+        $this->load->model('gallery/frontend/media_model', 'frontend_media');
+        $medias = $this->frontend_media->get_all(['widget_id' => $widget['widget_id']]);
+        $media_lenght = count($medias);
+        for ($i = 0; $i < $media_lenght; $i++) {
+            $medias[$i]['media_config'] = (array) json_decode($medias[$i]['media_config']);
+        }
+        $this->load->section($widget['position_name'], ['data' => $medias]);
     }
 
 }
